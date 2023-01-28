c6ad39d58af037881b4597d1c8f901d84124736a
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 22:45:20 -0500

    operadores_aritmeticos
    Creación de proyecto

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,34 @@
+print("suma:")
+
+numero_uno = 5
+numero_dos = 6
+
+suma = numero_uno + numero_dos
+print("Resultado es: " + str(suma))
+
+print("resta:")
+
+resta = numero_uno - numero_dos
+print("Resultado es: " + str(resta))
+
+print("Multiplicación")
+mult = numero_uno*numero_dos
+print("Resultado es: " + str(mult))
+
+print("Exponenciación")
+expo = numero_uno**numero_dos
+print("Resultado es: " + str(expo))
+
+print("División")
+div = numero_uno/numero_dos
+print("Resultado es: " + str(div))
+
+print("Módulo")
+mod = numero_uno%numero_dos
+print("Resultado es: " + str(mod))
+
+
+print("División entera")
+divEnt = numero_uno//numero_dos
+print("Resultado es: " + str(divEnt))
+
